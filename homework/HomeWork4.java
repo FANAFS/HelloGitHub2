6d35d0cdec5d34726501e96c7048f31e28a1289f
F.N <da105201911@gmail.com>
2020-01-18 14:48:35 +0800

    add test456

--- homework/HomeWork4.java
+++ homework/HomeWork4.java
@@ -23,6 +23,8 @@ public class HomeWork4 {
 //			System.out.print("\t"+a[i]);
 //		}
 		
+		//test 123
+		//test456
 		
 	/* 第二題
 	 * 請建立一個字串，經過程式執行後，輸入結果是反過來的 例如String s = “Hello World”，執行結果即為dlroW olleH*/
